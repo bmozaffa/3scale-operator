0a995201f818b6f298fd9b97811eda1122c06afb
Eguzki Astiz Lezaun <eastizle@redhat.com>
2019-05-28 20:49:32 +0200

    progress commit

--- pkg/3scale/amp/template/amp.go
+++ pkg/3scale/amp/template/amp.go
@@ -6,14 +6,9 @@ import (
 
 func NewAmpTemplate(options []string) *templatev1.Template {
 	//components := []Component{
-	//	NewAmpImages(options),
-	//	NewRedis(options),
-	//	NewBackend(options),
-	//	NewMysql(options),
 	//	NewMemcached(options),
 	//	NewSystem(options),
 	//	NewZync(options),
-	//	NewApicast(options),
 	//	NewWildcardRouter(options),
 	//}
 
@@ -22,38 +17,14 @@ func NewAmpTemplate(options []string) *templatev1.Template {
 		adapters.NewRedisAdapter(options),
 		adapters.NewApicastAdapter(options),
 		adapters.NewBackendAdapter(options),
+		adapters.NewMysqlAdapter(options),
 	}
 
 	return Generator(adapterList)
-}
-
-func (ampTemplate *AmpTemplate) AssembleIntoTemplate(template *templatev1.Template, otherComponents []Component) {
-	for componentIdx := range ampTemplate.components {
-		ampTemplate.components[componentIdx].AssembleIntoTemplate(template, otherComponents)
-	}
-	ampTemplate.setTemplateFields(template)
-}
-
-func (ampTemplate *AmpTemplate) PostProcess(template *templatev1.Template, otherComponents []Component) {
-	for componentIdx := range ampTemplate.components {
-		ampTemplate.components[componentIdx].PostProcess(template, otherComponents)
-	}
 }
 
 func (ampTemplate *AmpTemplate) setTemplateFields(template *templatev1.Template) {
 	template.ObjectMeta.Name = "3scale-api-management"
 	template.ObjectMeta.Annotations = ampTemplate.buildAmpTemplateMetaAnnotations()
 	template.Message = "Login on https://${TENANT_NAME}-admin.${WILDCARD_DOMAIN} as ${ADMIN_USERNAME}/${ADMIN_PASSWORD}"
-}
-
-func (ampTemplate *AmpTemplate) buildAmpTemplateMetaAnnotations() map[string]string {
-	annotations := map[string]string{
-		"openshift.io/display-name":          "3scale API Management",
-		"openshift.io/provider-display-name": "Red Hat, Inc.",
-		"iconClass":                          "icon-3scale",
-		"description":                        "3scale API Management main system",
-		"tags":                               "integration, api management, 3scale",
-	}
-
-	return annotations
 }
